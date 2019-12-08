111243089179db3bde120e348e341d4997cc77f1
b-fischer <fischer_baptiste@hotmail.fr>
2019-12-08 17:34:44 +0100

    chore: CORS

--- src/main/java/tp/bfischer/post/PostController.java
+++ src/main/java/tp/bfischer/post/PostController.java
@@ -12,6 +12,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PostController {
 	
 	private final static Logger LOGGER = Logger.getLogger(PostController.class);
